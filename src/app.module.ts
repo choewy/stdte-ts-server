a151434fe51f73035125239a326b8e7b309f7a37
choewy <choewy32@gmail.com>
2023-11-05 19:29:23 +0900

    feature/app module

---
+++ src/app.module.ts
@@ -0,0 +1,23 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+import { AppController } from '@server/app.controller';
+import { AppService } from '@server/app.service';
+
+import { MySqlConfig, entireEntity } from '@server/common';
+import { CookieModule, SignModule } from '@server/core';
+import { AuthModule } from '@server/module';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot(new MySqlConfig().getTypeOrmModuleOptions(entireEntity)),
+    CookieModule,
+    SignModule,
+    AuthModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
---
+++ src/main.ts
@@ -0,0 +1,18 @@
+import express from 'express';
+
+import { NestFactory } from '@nestjs/core';
+
+import { CorsConfig } from '@server/common';
+import { AppModule } from '@server/app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: true }));
+  app.enableCors(new CorsConfig().getCorsOptions());
+
+  await app.listen(8000);
+}
+
+bootstrap();
