5beaa26477ae2614b76e52b797387265399cf6bc
choewy <choewy@trineedle.com>
2023-11-09 22:04:53 +0900

    feature/use swagger in team module

--- src/module/team/dto/create-team.body.dto.ts
+++ src/module/team/dto/create-team.body.dto.ts
@@ -1,16 +1,21 @@
 import { IsArray, IsInt, IsNotEmpty, IsOptional, IsString, Length, Min } from 'class-validator';
 
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
+
 export class CreateTeamBodyDto {
+  @ApiProperty({ type: String })
   @IsNotEmpty()
   @IsString()
   @Length(50)
   name: string;
 
+  @ApiPropertyOptional({ type: Number })
   @IsOptional()
   @IsInt()
   @Min(1)
   leaderId?: number;
 
+  @ApiPropertyOptional({ type: Number, isArray: true, example: [] })
   @IsOptional()
   @IsArray()
   @IsInt({ each: true })
--- src/module/team/team.controller.ts
+++ src/module/team/team.controller.ts
@@ -1,40 +1,50 @@
 import { Body, Controller, Delete, Get, Param, Patch, Post, Query } from '@nestjs/common';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
 
 import { ListQueryDto } from '@server/common';
+import { UseSignGuard } from '@server/core';
 
 import { TeamService } from './team.service';
 import { GetTeamParamDto, CreateTeamBodyDto, UpdateTeamBodyDto } from './dto';
 
+@UseSignGuard()
+@ApiTags('teams')
 @Controller('teams')
 export class TeamController {
   constructor(private readonly teamService: TeamService) {}
 
   @Get()
+  @ApiOperation({ summary: 'get team list' })
   async getTeamList(@Query() query: ListQueryDto) {
     return;
   }
 
   @Get(':id(\\d+)')
+  @ApiOperation({ summary: 'get team details' })
   async getTeamById(@Param() param: GetTeamParamDto) {
     return;
   }
 
   @Get(':id(\\d+)/members')
+  @ApiOperation({ summary: 'get team members' })
   async getTeamMembersById(@Param() param: GetTeamParamDto) {
     return;
   }
 
   @Post()
+  @ApiOperation({ summary: 'create team' })
   async createTeam(@Body() body: CreateTeamBodyDto) {
     return;
   }
 
   @Patch(':id(\\d+)')
+  @ApiOperation({ summary: 'update team' })
   async updateTeam(@Param() param: GetTeamParamDto, @Body() body: UpdateTeamBodyDto) {
     return;
   }
 
   @Delete(':id(\\d+)')
+  @ApiOperation({ summary: 'delete team' })
   async deleteTeam(@Param() param: GetTeamParamDto) {
     return;
   }
