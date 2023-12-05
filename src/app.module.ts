e1b0fea1251613bbb58b2bc4d69a66534ac0e763
choewy <choewy@trineedle.com>
2023-12-06 04:21:47 +0900

    feature/configs

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MySQLConfig } from './config';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot(), TypeOrmModule.forRoot(new MySQLConfig().getTypeOrmModuleOptions())],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/config/mysql.config.ts
@@ -0,0 +1,30 @@
+import { ConfigService } from '@nestjs/config';
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+
+import { entityTargets } from '@stdte-ts-entity';
+
+export class MySQLConfig {
+  private readonly configService = new ConfigService();
+
+  private readonly HOST = this.configService.get<string>('MYSQL_HOST');
+  private readonly PORT = this.configService.get<string>('MYSQL_PORT');
+  private readonly USERNAME = this.configService.get<string>('MYSQL_USERNAME');
+  private readonly PASSWORD = this.configService.get<string>('MYSQL_PASSWORD');
+  private readonly DATABASE = this.configService.get<string>('MYSQL_DATABASE');
+  private readonly TIMEZONE = this.configService.get<string>('MYSQL_TIMEZONE');
+
+  getTypeOrmModuleOptions(): TypeOrmModuleOptions {
+    return {
+      type: 'mysql',
+      host: this.HOST,
+      port: Number(this.PORT),
+      username: this.USERNAME,
+      password: this.PASSWORD,
+      database: this.DATABASE,
+      timezone: this.TIMEZONE,
+      entities: entityTargets,
+      autoLoadEntities: true,
+      logging: true,
+    };
+  }
+}
