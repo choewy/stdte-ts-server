095f598c20c8a804667e3b935c7e1c078525d362
choewy <choewy32@gmail.com>
2023-12-06 21:59:49 +0900

    feature/validate exception

--- src/common/exceptions.ts
+++ src/common/exceptions.ts
@@ -4,7 +4,20 @@ import {
   ForbiddenException,
   NotFoundException,
   UnauthorizedException,
+  ValidationError,
 } from '@nestjs/common';
+
+export class ValidationException extends BadRequestException {
+  constructor(errors: ValidationError[]) {
+    super();
+
+    const error = errors[0];
+
+    if (error) {
+      this.cause = Object.values(error.constraints ?? {});
+    }
+  }
+}
 
 export class CannotAccessException extends ForbiddenException {}
 export class AlreadyUsedUserEmailException extends ConflictException {}
--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { CorsConfig } from './config';
 import { HttpExceptionFilter } from './core';
+import { ValidationException } from './common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -16,6 +17,7 @@ async function bootstrap() {
   app.useGlobalFilters(app.get(HttpExceptionFilter));
   app.useGlobalPipes(
     new ValidationPipe({
+      exceptionFactory: (errors) => new ValidationException(errors),
       transform: true,
       transformOptions: {
         enableCircularCheck: true,
