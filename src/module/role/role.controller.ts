8f93be720207907ee616c87eb62f575bdc6ee4ee
choewy <choewy32@gmail.com>
2023-12-06 19:03:29 +0900

    refactor/role controller policy level

--- src/module/role/role.controller.ts
+++ src/module/role/role.controller.ts
@@ -2,38 +2,42 @@ import { Body, Controller, Delete, Get, Param, Patch, Post, Put, Query, UseGuard
 
 import { PolicyLevel } from '@entity';
 import { SetPolicyLevel } from '@server/common';
-import { JwtGuard, RoleGuard } from '@server/core';
+import { CredentialsGuard, JwtGuard, RoleGuard } from '@server/core';
 
 import { RoleService } from './role.service';
 import { CreateRoleBodyDto, RoleListQueryDto, RoleParamDto, UpdateRoleBodyDto, UpdateRoleUserBodyDto } from './dto';
 
-@SetPolicyLevel({ accessRoleLevel: PolicyLevel.Admin })
-@UseGuards(JwtGuard, RoleGuard)
+@UseGuards(JwtGuard, CredentialsGuard, RoleGuard)
 @Controller('roles')
 export class RoleController {
   constructor(private readonly roleService: RoleService) {}
 
   @Get()
+  @SetPolicyLevel({ accessRoleLevel: PolicyLevel.Read })
   async getRoles(@Query() query: RoleListQueryDto) {
     return this.roleService.getRoles(query);
   }
 
   @Post()
+  @SetPolicyLevel({ accessRoleLevel: PolicyLevel.Write })
   async createRole(@Body() body: CreateRoleBodyDto) {
     return this.roleService.createRole(body);
   }
 
   @Patch(':id(\\d+)')
+  @SetPolicyLevel({ accessRoleLevel: PolicyLevel.Update })
   async updateRole(@Param() param: RoleParamDto, @Body() body: UpdateRoleBodyDto) {
     return this.roleService.updateRole(param, body);
   }
 
   @Put(':id(\\d+)')
+  @SetPolicyLevel({ accessRoleLevel: PolicyLevel.Update })
   async updateRoleUsers(@Param() param: RoleParamDto, @Body() body: UpdateRoleUserBodyDto) {
     return this.roleService.updateRoleUsers(param, body);
   }
 
   @Delete(':id(\\d+)')
+  @SetPolicyLevel({ accessRoleLevel: PolicyLevel.Delete })
   async deleteRole(@Param() param: RoleParamDto) {
     return this.roleService.deleteRole(param);
   }
