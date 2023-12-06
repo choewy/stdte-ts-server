d0f17845df4eb489c63d3c8d9dd258c7218f866c
choewy <choewy32@gmail.com>
2023-12-06 12:07:29 +0900

    feature/cors options

---
+++ src/config/cors.config.ts
@@ -0,0 +1,15 @@
+import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
+import { ConfigService } from '@nestjs/config';
+
+export class CorsConfig {
+  private readonly configService = new ConfigService();
+
+  private readonly ORIGIN = this.configService.get<string>('CORS_ORIGIN');
+
+  getCorsOptions(): CorsOptions {
+    return {
+      origin: new RegExp(this.ORIGIN ?? ''),
+      credentials: true,
+    };
+  }
+}
