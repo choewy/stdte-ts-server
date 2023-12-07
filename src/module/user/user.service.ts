ae4e3ec006964af709cd450b8137cd33f298ad41
choewy <choewy32@gmail.com>
2023-12-07 14:54:50 +0900

    refactor/user module

--- src/module/user/user.service.ts
+++ src/module/user/user.service.ts
@@ -1,8 +1,10 @@
 import { DataSource } from 'typeorm';
 
 import { Injectable } from '@nestjs/common';
-import { UserListQueryDto } from './dto';
+
 import { ListDto, ResponseDto, UserQuery } from '@server/common';
+
+import { UserListQueryDto } from './dto';
 
 @Injectable()
 export class UserService {
@@ -10,7 +12,7 @@ export class UserService {
 
   async getUsers(query: UserListQueryDto) {
     return new ResponseDto(
-      new ListDto(query, await new UserQuery(this.dataSource).findUsersAsList(query.take, query.skip)),
+      new ListDto(query, await new UserQuery(this.dataSource).findUsersAsList(query.skip, query.take)),
     );
   }
 }
