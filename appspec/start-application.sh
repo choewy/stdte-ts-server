9408706d6fe3688a5e145c776b01c7480e0637ec
choewy <choewy32@gmail.com>
2023-12-08 20:56:21 +0900

    fix/start application script

--- appspec/start-application.sh
+++ appspec/start-application.sh
@@ -9,13 +9,13 @@ if [ "$(sudo docker container inspect --format '{{.Name}}' $BLUE 2>&1)" == "/$BL
   sudo docker rm -f $BLUE
 fi
 
-sudo docker run -d -p 3001:3000 --restart --name $BLUE $IMAGE_NAME
+sudo docker run -d -p 3001:3000 --restart=always --name $BLUE $IMAGE_NAME
 
 
 if [ "$(sudo docker container inspect --format '{{.Name}}' $GREEN 2>&1)" == "/$GREEN" ]; then
   sudo docker rm -f $GREEN
 fi
 
-sudo docker run -d -p 3002:3000 --restart --name $GREEN $IMAGE_NAME
+sudo docker run -d -p 3002:3000 --restart=always --name $GREEN $IMAGE_NAME
 
 exit 0
