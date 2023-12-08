2033ff85d639c4e0015b48e71eb5857860a36f2e
choewy <choewy32@gmail.com>
2023-12-08 22:36:15 +0900

    fix/replace prepare to init

--- docker/server/scripts/build.sh
+++ docker/server/scripts/build.sh
@@ -2,10 +2,13 @@
 
 IMAGE_NAME="$1"
 
-if ! [ -d "build" ]; then
-  mkdir build
+if [ -d "build" ]; then
+  rm -rf build
 fi
 
+mkdir build
+
+cp -r ../../package* build
 cp -r ../../dist build
 cp ../../.env build
 
