04e123f2a831ab6cd456a48aff16a9d379ce673d
choewy <choewy32@gmail.com>
2023-12-11 23:34:25 +0900

    feature/io redis adapter

--- src/config/index.ts
+++ src/config/index.ts
@@ -2,6 +2,7 @@ export * from './system.config';
 export * from './app.config';
 export * from './cors.config';
 export * from './jwt.config';
+export * from './redis.config';
 export * from './mysql.config';
 export * from './credentials.config';
 export * from './aws.config';
