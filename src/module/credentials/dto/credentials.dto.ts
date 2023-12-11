cacad92e2f08875de1d0b0c48f1d1a7c71a5d512
choewy <choewy32@gmail.com>
2023-12-12 02:44:32 +0900

    feature/credentials dto

--- src/module/credentials/dto/credentials.dto.ts
+++ src/module/credentials/dto/credentials.dto.ts
@@ -3,12 +3,14 @@ import { Credentials, CredentialsStatus } from '@entity';
 import { toISOString } from '@server/common';
 
 export class CredentialsDto {
+  id: number;
   email: string;
   status: CredentialsStatus;
   createdAt: string | null;
   updatedAt: string | null;
 
   constructor(credentials: Credentials) {
+    this.id = credentials.id;
     this.email = credentials.email;
     this.status = credentials.status;
     this.createdAt = toISOString(credentials.createdAt);
