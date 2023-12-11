91d94db81e944d2956c205dd5a77021480761753
choewy <choewy32@gmail.com>
2023-12-11 20:18:35 +0900

    feature/time memo module

---
+++ src/module/time/time-memo/dto/time-memo.dto.ts
@@ -0,0 +1,19 @@
+import { TimeMemo } from '@entity';
+
+import { toISOString } from '@server/common';
+
+export class TimeMemoDto {
+  id: string;
+  date: string;
+  text: string;
+  createdAt: string | null;
+  updatedAt: string | null;
+
+  constructor(timeMemo: TimeMemo) {
+    this.id = timeMemo.id;
+    this.date = timeMemo.date;
+    this.text = timeMemo.text ?? '';
+    this.createdAt = toISOString(timeMemo.createdAt);
+    this.updatedAt = toISOString(timeMemo.updatedAt);
+  }
+}
