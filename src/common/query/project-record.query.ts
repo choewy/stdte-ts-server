f0faf57c4ad12579bf142b568e0cd2e226f0cc23
choewy <choewy32@gmail.com>
2023-12-19 17:42:43 +0900

    feature/create or update project with project order, sale records

--- src/common/query/project-record.query.ts
+++ src/common/query/project-record.query.ts
@@ -11,16 +11,19 @@ export class ProjectRecordQuery {
     this.projectSaleRecordRepository = connection.getRepository(ProjectSaleRecord);
   }
 
-  async insertProjectRecords(projectId: number) {
-    await this.projectOrderRecordRepository.insert(this.projectOrderRecordRepository.create({ projectId }));
-    await this.projectSaleRecordRepository.insert(this.projectSaleRecordRepository.create({ projectId }));
+  async insertProjectOrderRecord(projectId: number, entity: DeepPartial<ProjectOrderRecord>) {
+    await this.projectOrderRecordRepository.insert(this.projectOrderRecordRepository.create({ projectId, ...entity }));
+  }
+
+  async insertProjectSaleRecord(projectId: number, entity: DeepPartial<ProjectSaleRecord>) {
+    await this.projectOrderRecordRepository.insert(this.projectOrderRecordRepository.create({ projectId, ...entity }));
   }
 
   async updateProjectOrderRecord(projectId: number, entity: DeepPartial<ProjectOrderRecord>) {
-    return this.projectOrderRecordRepository.update({ projectId }, entity);
+    return this.projectOrderRecordRepository.update({ projectId }, this.projectOrderRecordRepository.create(entity));
   }
 
   async updateProjectSaleRecord(projectId: number, entity: DeepPartial<ProjectOrderRecord>) {
-    return this.projectSaleRecordRepository.update({ projectId }, entity);
+    return this.projectSaleRecordRepository.update({ projectId }, this.projectSaleRecordRepository.create(entity));
   }
 }
--- src/module/project/project.service.ts
+++ src/module/project/project.service.ts
@@ -63,16 +63,39 @@ export class ProjectService {
       const taskMainCategoryQuery = new TaskMainCategoryQuery(em);
 
       const insert = await projectQuery.insertProject({
-        ...body,
+        name: body.name,
+        code: body.code,
+        description: body.description,
+        difficulty: body.difficulty,
+        amount: body.amount,
+        status: body.status,
+        startDate: body.startDate,
+        endDate: body.endDate,
+        keepDate: body.keepDate,
         customer: await customerQuery.findCustomerOnlyId(body.customer),
         businessCategory: await businessCategoryQuery.findBusinessCategoryOnlyId(body.businessCategory),
         industryCategory: await industryCategoryQuery.findIndustryCategoryOnlyId(body.industryCategory),
         taskMainCategory: await taskMainCategoryQuery.findTaskMainCategoryOnlyId(body.taskCategory),
+        canExpose: body.canExpose,
       });
 
       const projectId = insert.raw.insertId;
-      await projectRecordQuery.insertProjectRecords(projectId);
-      await projectUsersQuery.updateProjectUsers(projectId, body);
+      await projectRecordQuery.insertProjectOrderRecord(projectId, {
+        date: body.orderRecordDate,
+        amount: body.orderRecordAmount,
+      });
+      await projectRecordQuery.insertProjectSaleRecord(projectId, {
+        date: body.saleRecordDate,
+        amount: body.saleRecordAmount,
+      });
+      await projectUsersQuery.updateProjectUsers(projectId, {
+        externalOwners: body.externalOwners,
+        externalManagers: body.externalManagers,
+        externalLeaders: body.externalLeaders,
+        internalOwners: body.internalOwners,
+        internalManagers: body.internalManagers,
+        internalLeaders: body.internalLeaders,
+      });
 
       return projectId;
     });
@@ -102,6 +125,7 @@ export class ProjectService {
 
     await this.dataSource.transaction(async (em) => {
       const projectQuery = new ProjectQuery(em);
+      const projectRecordQuery = new ProjectRecordQuery(em);
       const projectUsersQuery = new ProjectUsersQuery(em);
       const customerQuery = new CustomerQuery(em);
       const businessCategoryQuery = new BusinessCategoryQuery(em);
@@ -128,9 +152,25 @@ export class ProjectService {
         taskMainCategory: body.taskCategory
           ? await taskMainCategoryQuery.findTaskMainCategoryOnlyId(body.taskCategory)
           : undefined,
+        canExpose: body.canExpose,
       });
 
-      await projectUsersQuery.updateProjectUsers(param.id, body);
+      await projectRecordQuery.updateProjectOrderRecord(param.id, {
+        date: body.orderRecordDate,
+        amount: body.orderRecordAmount,
+      });
+      await projectRecordQuery.updateProjectSaleRecord(param.id, {
+        date: body.saleRecordDate,
+        amount: body.saleRecordAmount,
+      });
+      await projectUsersQuery.updateProjectUsers(param.id, {
+        externalOwners: body.externalOwners,
+        externalManagers: body.externalManagers,
+        externalLeaders: body.externalLeaders,
+        internalOwners: body.internalOwners,
+        internalManagers: body.internalManagers,
+        internalLeaders: body.internalLeaders,
+      });
     });
 
     const project = await projectQuery.findProjectById(param.id);
