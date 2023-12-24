0d8d1573f1d95bef1fb7afcb7067f739c6ebca20
choewy <choewy32@gmail.com>
2023-12-24 22:40:24 +0900

    refactor/remove unused codes

--- src/common/query/project-record.query.ts
+++ src/common/query/project-record.query.ts
@@ -133,53 +133,6 @@ export class ProjectRecordQuery {
     });
   }
 
-  async findProjectSaleRecordAnalysis(args: DateRangeArgs) {
-    const queryBuilder = this.projectSaleRecordRepository
-      .createQueryBuilder('row')
-      .innerJoin('row.project', 'project')
-      .select('SUM(row.amount)', 'amount')
-      .where('row.date >= :s', { s: args.s })
-      .andWhere('row.date <= :e', { e: args.e });
-
-    const totalQueryBuilder = queryBuilder.clone();
-    const yearQueryBuilder = queryBuilder.clone().addSelect('DATE_FORMAT(row.date, "%Y" )', 'year').groupBy('year');
-
-    const customerQueryBuilder = queryBuilder
-      .clone()
-      .innerJoin('project.customer', 'customer')
-      .addSelect('DATE_FORMAT(row.date, "%Y" )', 'year')
-      .addSelect('customer.id', 'id')
-      .addSelect('customer.alias', 'row')
-      .groupBy('customer.id')
-      .addGroupBy('year');
-
-    const businessCategoryQueryBuilder = queryBuilder
-      .clone()
-      .innerJoin('project.businessCategory', 'businessCategory')
-      .addSelect('DATE_FORMAT(row.date, "%Y" )', 'year')
-      .addSelect('businessCategory.id', 'id')
-      .addSelect('businessCategory.name', 'row')
-      .groupBy('businessCategory.id')
-      .addGroupBy('year');
-
-    const industryCategoryQueryBuilder = queryBuilder
-      .clone()
-      .innerJoin('project.industryCategory', 'industryCategory')
-      .addSelect('DATE_FORMAT(row.date, "%Y" )', 'year')
-      .addSelect('industryCategory.id', 'id')
-      .addSelect('industryCategory.name', 'row')
-      .groupBy('industryCategory.id')
-      .addGroupBy('year');
-
-    return Promise.all([
-      totalQueryBuilder.getRawOne<{ amount: string }>(),
-      yearQueryBuilder.getRawMany<{ year: string; amount: string }>(),
-      customerQueryBuilder.getRawMany<ProjectRecordAnalysisRaw>(),
-      businessCategoryQueryBuilder.getRawMany<ProjectRecordAnalysisRaw>(),
-      industryCategoryQueryBuilder.getRawMany<ProjectRecordAnalysisRaw>(),
-    ]);
-  }
-
   async insertProjectOrderRecord(entity: DeepPartial<ProjectOrderRecord>) {
     return this.projectOrderRecordRepository.insert(this.projectOrderRecordRepository.create(entity));
   }
