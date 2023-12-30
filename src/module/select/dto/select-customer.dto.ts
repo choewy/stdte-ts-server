f7c6ac43d1c560cc273d36894bb3563004a83747
choewy <choewy32@gmail.com>
2023-12-30 14:54:33 +0900

    refactor/customer select dto

--- src/module/select/dto/select-customer.dto.ts
+++ src/module/select/dto/select-customer.dto.ts
@@ -4,6 +4,6 @@ import { SelectDto } from './select.dto';
 
 export class SelectCustomerDto extends SelectDto {
   constructor(customer: Customer) {
-    super(customer.id, `${customer.alias}(${[customer.kr, customer.en].join(', ')})`);
+    super(customer.id, customer.alias);
   }
 }
