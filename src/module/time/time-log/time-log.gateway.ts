ab7d01afc5b946580753ef6edf6eb476b2cc119e
choewy <choewy32@gmail.com>
2023-12-31 15:41:57 +0900

    feature/time log gateway

--- src/module/time/time-log/time-log.gateway.ts
+++ src/module/time/time-log/time-log.gateway.ts
@@ -1,14 +1,10 @@
-import cookie from 'cookie';
-
-import { IncomingMessage } from 'http';
 import { Namespace, Socket } from 'socket.io';
 
 import { OnEvent } from '@nestjs/event-emitter';
 import { OnGatewayConnection, WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
 
 import { TimeLogEvent } from '@server/common';
-import { CorsConfig } from '@server/config';
-import { CookieKey, JwtService, JwtTokenType } from '@server/core';
+import { CookieKey, Gateway, JwtService, JwtTokenType } from '@server/core';
 
 import { TimeLogService } from './time-log.service';
 import { TimeLogGatewayEvent } from './enums';
@@ -16,11 +12,7 @@ import { TimeLogGatewayEvent } from './enums';
 @WebSocketGateway({
   namespace: 'timelog',
   transports: ['websocket'],
-  allowRequest: (req: IncomingMessage, func) => {
-    const pass = new CorsConfig().checkOrigin(req.headers.origin ?? '');
-
-    func(null, pass);
-  },
+  allowRequest: Gateway.allowRequest,
 })
 export class TimeLogGateway implements OnGatewayConnection {
   @WebSocketServer()
@@ -30,7 +22,7 @@ export class TimeLogGateway implements OnGatewayConnection {
   constructor(private readonly timeLogService: TimeLogService) {}
 
   handleConnection(client: Socket) {
-    const cookies = cookie.parse(client.handshake.headers.cookie ?? '');
+    const cookies = Gateway.parseCookies(client);
     const result = this.jwtService.verify(JwtTokenType.Access, cookies[CookieKey.Access]);
 
     if (result.payload == null) {
