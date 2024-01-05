2c9c81ed608a2faa849df0462f820638952d3d27
choewy <choewy32@gmail.com>
2024-01-05 14:28:32 +0900

    feature/task category select dto

--- src/module/select/dto/select-task-category.dto.ts
+++ src/module/select/dto/select-task-category.dto.ts
@@ -3,7 +3,11 @@ import { TaskMainCategory } from '@entity';
 import { SelectDto } from './select.dto';
 
 export class SelectTaskCategoryDto extends SelectDto {
+  isDefault: boolean;
+
   constructor(taskMainCategory: TaskMainCategory) {
     super(taskMainCategory.id, taskMainCategory.name);
+
+    this.isDefault = taskMainCategory.isDefault;
   }
 }
